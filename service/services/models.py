f12a06edff9714b40b95fd4124c10c437e70e725
zorgiy <91011665+zorgiy@users.noreply.github.com>
2023-01-12 16:28:18 +0500

    Day 2 - 1

---
+++ service/services/models.py
@@ -0,0 +1,35 @@
+from django.core.validators import MaxValueValidator
+from django.db import models
+
+from clients.models import Client
+
+
+class Service(models.Model):
+    name = models.CharField(max_length=50)
+    full_price = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f'{self.name} : {self.full_price}$'
+
+
+class Plan(models.Model):
+    PLAN_TYPES = (
+        ('full', 'Full'),
+        ('student', 'Student'),
+        ('discount', 'Discount')
+    )
+
+    def __str__(self):
+        return f'{self.plan_type} : {self.discount_percent}%'
+
+    plan_type = models.CharField(choices=PLAN_TYPES, max_length=10)
+    discount_percent = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
+
+
+class Subscription(models.Model):
+    client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
+    service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
+    plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f'{self.client} - {self.service} ({self.plan})'
