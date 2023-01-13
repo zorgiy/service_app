9f24af283573b0acf54a2926267ab2c3379a8f30
zorgiy <91011665+zorgiy@users.noreply.github.com>
2023-01-13 15:44:26 +0500

    Day 4 -1

--- service/services/views.py
+++ service/services/views.py
@@ -9,6 +9,7 @@ from services.serializers import SubscriptionSerializers
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
         'plan',
+        'service',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name', 'user__email'))
     )
     serializer_class = SubscriptionSerializers
